8f942dabb71262137c92b5429343387f6021b002
b1gcat <b1gcat@b1gcat.local>
2024-10-16 12:03:56 +0800

    update

--- Makefile
+++ Makefile
@@ -10,11 +10,12 @@ ldFlag=-s -w -X main.Version=$(Version)  -X main.AppName=$(AppName) -X main.AppI
 all:win darwin
 	
 darwin:
+
 	rm -f *.syso
 	CGO_ENABLED=1 go build -ldflags "$(ldFlag)" -o dist/$(AppName)
 	
 win: icon_win
-	CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 go build -ldflags "$(ldFlag) -H=windowsgui" -o dist/$(AppName).exe 
+	CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 go build -ldflags "$(ldFlag) -H=windowsgui" -o dist/$(AppName)_$(Version).exe 
 	rm -rf *.syso
 	
 icon_win:
--- ai.go
+++ ai.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"net/http"
-	"net/url"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
-	"github.com/ollama/ollama/api"
+	"github.com/langgenius/dify-sdk-go"
 )
 
 func runAIChat(_ fyne.Window) {
@@ -26,52 +22,20 @@ func runAIChat(_ fyne.Window) {
 	result.SetMinRowsVisible(15)
 
 	address := widget.NewEntry()
-	address.SetText("localhost:11434")
-
-	model := widget.NewSelectEntry([]string{})
-	v1 := container.NewBorder(layout.NewSpacer(), layout.NewSpacer(),
-		widget.NewLabel("模型"), layout.NewSpacer(), model)
-
-	var client *api.Client
-	var connect *widget.Button
-	connect = widget.NewButton("连接", func() {
-		base, err := url.Parse("http://" + address.Text)
-		if err != nil {
-			result.Append("[-] " + err.Error() + "\n")
-			return
-		}
-
-		client = api.NewClient(base, &http.Client{})
-		resp, err := client.List(context.Background())
-		if err != nil {
-			result.Append("[-] " + err.Error() + "\n")
-			return
-		}
-
-		models := make([]string, 0)
-		for _, m := range resp.Models {
-			models = append(models, m.Name)
-			result.Append("[+]" + m.Name + "\n")
-		}
-
-		if len(models) == 0 {
-			result.Append("[-]" + "无模型" + "\n")
-			return
-		}
-
-		model.SetOptions(models)
-		model.SetText(models[0])
-		connect.Disable()
-		defer connect.Enable()
-
-	})
-
+	address.SetText("http://localhost")
 	v0 := container.NewBorder(layout.NewSpacer(), layout.NewSpacer(),
-		widget.NewLabel("大模型API地址"), connect, address)
+		widget.NewLabel("API地址"), layout.NewSpacer(), address)
+
+	key := widget.NewEntry()
+	key.SetText("app-gureJ1ADylgxZb9FYV1Y5MNy")
+
+	v1 := container.NewBorder(layout.NewSpacer(), layout.NewSpacer(),
+		widget.NewLabel("API密钥"), layout.NewSpacer(), key)
 
 	question := widget.NewMultiLineEntry()
 	question.SetMinRowsVisible(10)
-	question.SetText("你是谁")
+	question.SetText("介绍商用密码评估")
+
 	v2 := container.NewBorder(layout.NewSpacer(), layout.NewSpacer(),
 		widget.NewLabel("问题"), layout.NewSpacer(), question)
 
@@ -81,27 +45,43 @@ func runAIChat(_ fyne.Window) {
 	//
 	var send *widget.Button
 	send = widget.NewButton("发送", func() {
-		if client == nil {
-			dialog.ShowError(fmt.Errorf("未连接大模型"), w)
-			return
-		}
-		result.Append("[+] 使用模型" + model.Text + "\n")
-		err := client.Generate(context.Background(), &api.GenerateRequest{
-			Prompt: question.Text,
-			Model:  model.Text,
-		}, func(cr api.GenerateResponse) error {
-			result.Append(cr.Response)
-			if cr.Done {
-				result.Append("\n[+] 结束原因:" + cr.DoneReason)
-			}
-			return nil
-		})
-		if err != nil {
-			result.Append("[-] " + err.Error() + "\n")
-			return
-		}
 		send.Disable()
 		defer send.Enable()
+
+		var (
+			ctx = context.Background()
+			c   = dify.NewClient(address.Text, key.Text)
+
+			req = &dify.ChatMessageRequest{
+				Query: question.Text,
+				User:  "api-user",
+			}
+
+			ch  chan dify.ChatMessageStreamChannelResponse
+			err error
+		)
+
+		if ch, err = c.Api().ChatMessagesStream(ctx, req); err != nil {
+			return
+		}
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case streamData, isOpen := <-ch:
+				if err = streamData.Err; err != nil {
+					result.Append("\n" + err.Error() + "\n")
+					return
+				}
+				if !isOpen {
+					result.Append("\nEnd\n")
+					return
+				}
+
+				result.Append(streamData.Answer)
+			}
+		}
 
 	})
 
